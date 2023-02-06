59494fe3aea1994b8c456ca8cea7ebd928421fdd
Ru960413 <wendyhu0413@gmail.com>
2023-02-06 17:53:39 +0800

    ad initMap function

--- static/main-en.js
+++ static/main-en.js
@@ -1,28 +1,38 @@
 //Get user current position
 
-// 先確認使用者裝置能不能抓地點
+// making sure we can access geolocation through user's device
 if (navigator.geolocation) {
-  // 使用者不提供權限，或是發生其它錯誤
+  // if not successful:
   function error() {
     alert("Can't access your location");
   }
 
-  // 使用者允許抓目前位置，回傳經緯度
+  // if successful:
   function success(position) {
     console.log(position.coords.latitude, position.coords.longitude);
+    // return position.coords.latitude, position.coords.longitude;
   }
 
-  // 跟使用者拿所在位置的權限
   navigator.geolocation.getCurrentPosition(success, error);
 } else {
   alert("Sorry,your device doesn't support geolocation");
 }
 
 // set the center of the map to the user's current location
-let originPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+// let originPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
+let map;
+// let user_lat = position.coords.latitude;
+// let user_lng = position.coords.longitude;
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 24.1906155, lng: 120.5882385 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
 //add vet clinics as markers on google map(use loop)
 
-
-//use Distance matrix API to calculate the transport time between user and vet clinics(within 20 km?) Not Sure how 
+//use Distance matrix API to calculate the transport time between user and vet clinics(within 20 km?) Not Sure how
